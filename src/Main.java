f59b6184891fd220b379102e348252912f2d28ae
aturatbekova88 <aturatbekova88@gmail.com>
2025-10-15 22:11:52 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Triangle triangle = new Triangle();
+        triangle.a = 3;
+        triangle.b = 6;
+        triangle.c = 4;
+
+        triangle.area();
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+
+    int a;
+    int b;
+    int c;
+
+    public void area() {
+        System.out.println("S = "+a+" * "+b+" * "+c);
+        System.out.println("S = "+a * b * c);
+    }
+}
